d7bad568830ab11e9e392d395da94fafd83cccb7
Jiayu <colocolo6102@gmail.com>
2022-07-03 18:03:38 -0700

    update main and add song utils

---
+++ rythm/song_utils.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+
+class Songs:
+
+    def __init__(self):
+        """
+        each song contains a list of beat counts and its time sig,
+        each bar (measure) is separated by common in the list, so that their y position can be randomized
+        beatRatio = 4 means that each note height = 1/4 beat
+        the last item are the locations of suspended notes
+        """
+
+        # time signature: 4/4
+        self.kingOfKings = [{'bpm': 70, 'beatRatio': 4}, [26, 31],
+                            np.ones((8,)) * 2,
+                            np.ones((4,)) * 4,
+                            np.ones((8,)) * 2,
+                            np.ones((3,)) * 4, np.array([1, 2, 1, 1]),
+                            np.array([3, 1, 2, 1, 1]),
+                            np.array([3, 1, 2, 1])
+                            ]
+
+        self.names = ['King of Kings']
+        self.nSongs = len(self.names)
+
+
+def convertXY(song: Songs, note_size: int, top_player: int) -> list:
+    """
+    Convert counts to x and y position in the windows
+    :param note_size: height of notes
+    :param top_player: top y position of player
+    :param song: contains all counts for a song
+    :return: a list of notes x and y positions
+    """
+
+    raw_beats = song[2:]
+    beats = np.concatenate(raw_beats)
+    multiplier = np.zeros((len(beats),))
+
+    for i in range(len(beats) - 1):
+        multiplier[i + 1] = beats[i] + multiplier[i]
+
+    pos_y = [(top_player - 2 * note_size) - multiplier[i] * note_size for i in range(len(beats))]
+
+    # generate note pos by cycling through [100, 200, 300]
+    pos_x = np.zeros(beats.shape)
+    col_ = 100
+    start_ = 0
+    for bar in raw_beats:
+        temp_note_len = len(bar)
+        pos_x[start_:start_ + temp_note_len] = np.ones((temp_note_len,)) * col_
+        start_ += temp_note_len
+        col_ += 100
+        if col_ > 300: col_ = 100
+
+    return pos_x, pos_y
+
+
+
--- rythm/whata.py
+++ rythm/whata.py
@@ -5,6 +5,7 @@ import pygame as pg
 from sys import exit
 from random import choice
 import numpy as np
+from song_utils import *
 from GIFImage import GIFImage
 
 
@@ -41,6 +42,17 @@ class Note(pg.sprite.Sprite):
         self.value = 1
 
     def update(self):
+        """
+        The current rhythm system works as follows:
+        suppose we have a 60 bpm song, meaning 1 beat per second.
+        if each of our note corresponds to 1 beat (1 note : 1 beat, this ratio can change),
+        then it means that we need to 'kill' one note per second.
+        (The notes are killed when their bottom y touches the top y of the player.)
+        So to kill 1 notes/s, we need the note to travel exactly its height each second
+        suppose our game is running at 60 fps, meaning that 1/60 s will pass between each frame
+        so 33/1 (pixel/notes) * 1/1 (notes/beats) * 60/60 (beats/s) * 1/60 (s/frame) = 33/60 (pixel/frame)
+        so after 60 frames (which is 1 s), the note will travel 33 pixel (1 beat), which is what we want
+        """
         global dt, bps, note_height
         self.speed = dt * bps * note_height
         self.y_float += self.speed
@@ -65,71 +77,61 @@ class RedNote(Note):
         self.value = 3
 
 
-def initNotes(notes: object, note_size: float, top_flower: float):
+def initNotes(song: Songs, note_size: int, top_player: int):
     """
 
-    :param speed: falling speed of notes
-    :param notes: a pg.sprite.Group object that contains notes
-    :param note_size: height of the note
-    :param top_flower: the y coord of flower top rect
+    :param song: a list of counts
+    :param note_size: height of note
+    :param top_player: player top y position
     """
 
-    # number of notes at the start of the game
-    n_notes = int(np.floor((top_flower - note_size) / note_size))
-
-    # create some hidden notes
-    n_notes = n_notes + 5
-
-    # list of notes y pos (the bottom of rect)
-    note_ys = [(top_flower) - i * note_size * 2 for i in range(n_notes)]
+    sus = song[1]
+    pos_x, pos_y = convertXY(song, note_size, top_player)
 
     # render notes on screen
-    for i in range(n_notes):
-        notes.add(choice([Note([choice([100, 100, 100]), note_ys[i]]),
-                          Note([choice([100, 100, 100]), note_ys[i]])]))
-
-
-def initNotes_test(notes: object, note_size: float, top_flower: float):
-    # beats_raw = [np.ones((8,)),
-    #              np.ones((4,)) * 2,
-    #              np.ones((8,)),
-    #              np.ones((3,)) * 2, np.array([0.5, 1, 0.5]),
-    #              np.array([0.5, 1.5, 0.5, 1, 0.5, 0.5, 1.5, 0.5, 1, 0.5])]
-
-    beats_raw = [np.ones((8,)),
-                 np.ones((4,)) * 2,
-                 np.ones((8,)),
-                 np.ones((3,)) * 2, np.array([0.5, 1, 0.5, 0.5]),
-                 np.array([1.5, 0.5, 1, 0.5, 0.5]),
-                 np.array([1.5, 0.5, 1, 0.5])]
-
-    beats = np.concatenate(beats_raw)
-
-    multiplier = np.zeros((len(beats),))
-
-    pos_ = np.zeros(beats.shape)
-
-    col_ = 100
-    start_ = 0
-    for bar in beats_raw:
-        temp_note_len = len(bar)
-        pos_[start_:start_ + temp_note_len] = np.ones((temp_note_len,)) * col_
-        start_ += temp_note_len
-        col_ += 100
-        if col_ > 300: col_ = 100
-
-    for i in range(len(beats) - 1):
-        # 2 times the previous beats + previous multi
-        multiplier[i + 1] = 2 * beats[i] + multiplier[i]
-
-    # h = np.array([0, 2, 4, 6, 8, 10, 12, 14, 16, 20, 24, 28])
-    note_ys = [(top_flower - 2 * note_size) - multiplier[i] * note_size for i in range(len(beats))]
-    print(note_ys)
-
-    for i in range(len(beats)):
-        if i == 26 or i == 31:
+    for i in range(len(pos_y)):
+        if set(sus).intersection({i}):
             continue
-        notes.add(Note([pos_[i], note_ys[i]]))
+        notes.add(Note([pos_x[i], pos_y[i]]))
+
+
+# def initNotes_test(notes: object, note_size: float, top_flower: float):
+#
+#     # value of each rain drop
+#     # 1 beat gets a value of 2, 1/2 beat gets a value of 1
+#     beats_raw = [np.ones((8,)),
+#                  np.ones((4,)) * 2,
+#                  np.ones((8,)),
+#                  np.ones((3,)) * 2, np.array([0.5, 1, 0.5, 0.5]),
+#                  np.array([1.5, 0.5, 1, 0.5, 0.5]),
+#                  np.array([1.5, 0.5, 1, 0.5])]
+#
+#     beats = np.concatenate(beats_raw)
+#
+#     multiplier = np.zeros((len(beats),))
+#
+#     for i in range(len(beats) - 1):
+#         # 2 times the previous beats + previous multi
+#         multiplier[i + 1] = 2 * beats[i] + multiplier[i]
+#
+#     # generate note pos by cycling through [100, 200, 300]
+#     pos_ = np.zeros(beats.shape)
+#     col_ = 100
+#     start_ = 0
+#     for bar in beats_raw:
+#         temp_note_len = len(bar)
+#         pos_[start_:start_ + temp_note_len] = np.ones((temp_note_len,)) * col_
+#         start_ += temp_note_len
+#         col_ += 100
+#         if col_ > 300: col_ = 100
+#
+#     note_ys = [(top_flower - 2 * note_size) - multiplier[i] * note_size for i in range(len(beats))]
+#
+#     for i in range(len(beats)):
+#         # pos of silent notes
+#         # if i == 26 or i == 31:
+#         #     continue
+#         notes.add(Note([pos_[i], note_ys[i]]))
 
 
 pg.init()
@@ -146,8 +148,10 @@ background.fill((255, 255, 255))
 screen.blit(background, (0, 0))
 
 # music
+songBase = Songs()
+KOKs = songBase.kingOfKings
 note_height = 33  # px
-bps = 280 / 60  # beats / s
+bps = KOKs[0]['bpm'] * KOKs[0]['beatRatio'] / 60  # beats / s
 dt = 17 / 1000  # ms
 
 bg_music = pg.mixer.Sound('bg5.wav')
@@ -170,8 +174,6 @@ notes = pg.sprite.Group()
 note_timer = pg.USEREVENT + 1
 pg.time.set_timer(note_timer, 350)
 
-# flower_gif = GIFImage("resized_flower.gif")
-
 game_start = True
 
 while True:
@@ -191,7 +193,8 @@ while True:
                 game_start = False
                 p1.score = 0
                 bg_music.play(loops=-1)
-                initNotes_test(notes, 33, 450)
+                # 450 is the player top y-pos
+                initNotes(KOKs, note_height, 450)
 
     if game_active:
         pg.mouse.set_visible(False)

66e54f605e6930ed652b09a472f95989fa41e377
xinyue-yin <70519655+xinyue-yin@users.noreply.github.com>
2022-07-22 16:38:52 -0700

    Update Player.py

--- rythm/Player.py
+++ rythm/Player.py
@@ -10,17 +10,21 @@ class Player(pg.sprite.Sprite):
 			pg.sprite.Sprite.__init__(self)
 			self.player_images_iter = self._load_images()
 			self.image = next(self.player_images_iter)
-			self.rect = self.images.get_rect()
+			self.rect = self.image.get_rect()
 			self.pre_x_pos = pg.mouse.get_pos()[0]
 			self.offset = (-100, -50)
-			self.originY = 500
+
+			#Y position of Player
+			self.originY = 550
 			self.score = 0
 			
 		def _load_images(self):
 			"""Loads images from the sprite sheet"""
 			filename = 'WalkSprite.png'
-			images_ss = SpriteSheet('WalkSprite.png', 153, 276)
-			player_images = images_ss.load_grid_images(7,1)
+
+			#Modify this to scale the images
+			images_ss = SpriteSheet('WalkSprite.png', bx=153)
+			player_images = images_ss.load_grid_images(7,1, x_margin = 20, y_margin = 20, y_padding = 40)
 			return itertools.cycle(player_images)
 		
 		def update(self):
@@ -41,4 +45,3 @@ class Player(pg.sprite.Sprite):
 			
 		def getScore(self):
 			return self.score
-			

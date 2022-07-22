0b7a946b598da69d2554934599400ae81772fda0
xinyue-yin <70519655+xinyue-yin@users.noreply.github.com>
2022-07-22 16:36:21 -0700

    Create Note.py

---
+++ rythm/Note.py
@@ -0,0 +1,38 @@
+import pygame as pg
+from SpriteSheet import SpriteSheet
+import itertools
+from random import choice
+
+class Note(pg.sprite.Sprite):
+	def __init__(self, start_pos):
+		pg.sprite.Sprite.__init__(self)
+		imageFile = choice(['FallingSheepUpsideDown.png', 'FallingSheep.png', 'FallingSheepRotating.png'])
+
+		#Modify this to change the scale of the SpriteSheet
+		self.image_iter = itertools.cycle(SpriteSheet(imageFile,150, 150).load_grid_images(2,1,x_margin =10, x_padding=25, y_margin =10, y_padding =15))
+
+		self.image = next(self.image_iter)
+		self.rect = self.image.get_rect(midbottom = (start_pos,0))
+		self.speed = 6
+		self.value = 1
+		self.counter = 0
+
+	def update(self):
+		self.rect.y += self.speed
+		self.counter += 1
+
+		#Counter to change to the next image
+		if self.counter > 8:
+			self.image = next(self.image_iter)
+			self.counter = 0
+
+	def caught(self, player):
+		if self.rect.colliderect(player.rect):
+			player.incScore(self.value)
+			self.kill()
+			return True
+		#Kill condition for notes
+		if self.rect.y > 500:
+			return False
+		else:
+			return True

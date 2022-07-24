ea394f0e92890dbbc7021a54390f15319852670e
xinyue-yin <70519655+xinyue-yin@users.noreply.github.com>
2022-07-23 21:28:04 -0700

    Update Note.py

--- rythm/Note.py
+++ rythm/Note.py
@@ -2,26 +2,23 @@ import pygame as pg
 from SpriteSheet import SpriteSheet
 import itertools
 from random import choice
-import whata
 
 class Note(pg.sprite.Sprite):
-	def __init__(self, start_pos, bpm):
+	def __init__(self, start_pos, speed):
 		pg.sprite.Sprite.__init__(self)
-		imageFile = choice(['FallingSheepUpsideDown.png', 'FallingSheep.png', 'FallingSheepRotating.png'])
+		imageFile = choice(['graphic/FallingSheepUpsideDown.png', 'graphic/FallingSheep.png', 'graphic/FallingSheepRotating.png'])
 
 		#Modify this to change the scale of the SpriteSheet
-		self.image_iter = itertools.cycle(SpriteSheet(imageFile,150, 150).load_grid_images(2,1,x_margin =10, x_padding=25, y_margin =10, y_padding =15))
+		self.image_iter = itertools.cycle(SpriteSheet(imageFile,150, 150).load_grid_images(2,1,x_margin =10, x_padding=25, y_margin =10, y_padding =25))
 
 		self.image = next(self.image_iter)
-		self.rect = self.image.get_rect(midbottom = (start_pos,0))
-		self.speed = 6
+		self.rect = self.image.get_rect(midbottom = (start_pos[0],start_pos[1]))
+		self.speed = speed
 		self.value = 1
 		self.counter = 0
 		self.y_float = float(self.rect.y)
-		self.bpm = bpm
 
 	def update(self):
-		self.speed = whata.dt * self.bpm/60 * self.rect.get_height() * whata.beatRatio
 		self.y_float += self.speed
 		self.rect.y = int(self.y_float)
 		self.counter += 1

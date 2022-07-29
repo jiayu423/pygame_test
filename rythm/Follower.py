6f110e3cc8acb6717af9bfdc9dc7b371802d25a7
Cindy Yin <cindy.yin@globalrelay.net>
2022-07-29 16:59:32 -0700

    Improve the update method for Follower

--- rythm/Follower.py
+++ rythm/Follower.py
@@ -3,6 +3,9 @@ from SpriteSheet import SpriteSheet
 import itertools
 
 class Follower(pg.sprite.Sprite):
+	numRow = 1
+	numColumn = None
+	numOfFollower = 0
 	def __init__(self, player):
 		pg.sprite.Sprite.__init__(self)
 		self.image_iter = itertools.cycle(SpriteSheet("graphic/FallingSheep.png",80).load_grid_images(2,1,x_margin =10, x_padding=20, y_margin =15, y_padding =30))
@@ -11,21 +14,26 @@ class Follower(pg.sprite.Sprite):
 		self.image.fill((0,0,0,0))
 		self.rect = self.image.get_rect(midbottom = (200,800))
 		self.player = player
+		self.blitImage = self.image
 
 	def update(self):
-		numRow = 1
-		numColumn = None
-		self.clear()
-		for i in range(0, self.player.getScore()//10):
-			if numColumn == 5:
-				numRow +=1
-			numColumn = i % 6
-			self.image.blit(next(self.image_iter), (numColumn * 60, 700 - numRow * 40))
-		self.rect.bottomleft = (pg.mouse.get_pos()[0]+15, 670)
+		if Follower.numOfFollower != self.player.getScore()//10:
+			Follower.numOfFollower = self.player.getScore()//10
+			Follower.numColumn = Follower.numOfFollower % 8-1
+			if Follower.numColumn == 6:
+				Follower.numRow +=1
+			self.blitImage.blit(next(self.image_iter), (Follower.numColumn * 60, 700 - Follower.numRow * 40))
 		if self.player.flip:
-			self.image = pg.transform.flip(self.image, True, False)
+			self.image = pg.transform.flip(self.blitImage, True, False)
 			self.rect.bottomright = (pg.mouse.get_pos()[0]-105, 670)
+		else:
+			self.image = self.blitImage
+			self.rect.bottomleft = (pg.mouse.get_pos()[0]+15, 670)
 
 	def clear(self):
 		self.image = pg.Surface([400,700]).convert_alpha()
 		self.image.fill((0,0,0,0))
+		self.blitImage = self.image
+		Follower.numRow = 1
+		Follower.numColumn = None
+		Follower.numOfFollower = 0
